5955f5927bd0b670570d1974665eaf2cf60f3843
Alex Reed <ch.alexandre.reed@gmail.com>
2019-06-19 23:05:25 +0200

    first react app

---
+++ src/components/gif.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} className="gif" onClick={this.handleClick} alt="le-gif"/>
+    );
+  }
+}
+
+export default Gif;
